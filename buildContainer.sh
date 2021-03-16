49bfa75952f66fd7285398c174dd8053ab8dd823
Patrick Haggerty <hagg@haggconsulting.com>
2021-03-16 16:04:06 -0500

    Rename app

--- buildContainer.sh
+++ buildContainer.sh
@@ -1,3 +1,3 @@
-read -p "Hello World container version (e.g. 1.0)? " version
+read -p "Hello World Demo container version (e.g. 1.0)? " version
 version=${version:-1.0}
-gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/hello-world:$version
+gcloud builds submit --tag gcr.io/$GOOGLE_CLOUD_PROJECT/hello-world-demo:$version
