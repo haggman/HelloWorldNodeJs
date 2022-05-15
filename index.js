a759d2112b2b76e3cb8106e6bffbeba1eff7e997
Patrick Haggerty <hagg@haggconsulting.com>
2022-05-15 10:15:47 -0500

    Moved target to top

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080;
+const target = process.env.TARGET || 'World';
 
 app.get('/', (req, res) => {
   console.log('Hello world received a request.');
-
-  const target = process.env.TARGET || 'World';
+  
   res.send(`Hello ${target}!`);
 });
-
 
 app.listen(port, () => {
   console.log('Hello world listening on port', port);
